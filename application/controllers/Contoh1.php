59ab13fa92a033f740eff2a2ff3a62d31944a389
fajarfakhriann <fajarfakhrian60@gmail.com>
2022-04-12 22:29:26 +0700

    Instalasi CI dan mengerjakan Latihan Contoh-1

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,9 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+	public function index()
+	{
+		echo "<h1> Perkenalan </h1>";
+		echo " Nama saya Fajar Fakhrian dan saya Tinggal di daerah Rawamangun ,olahraga yang saya sukai adalah sepakbola";
+	}
+}
